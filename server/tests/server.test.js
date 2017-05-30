75c978c0d2bc41547e96d565ecba1c415cba0c81
Ben Llanes <chtka.ow@gmail.com>
2017-05-30 00:57:15 -0700

    Added POST /todos test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,10 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
     var text = 'Test todo text';
@@ -27,4 +31,34 @@ describe('POST /todos', () => {
         }).catch((err) => done(e));
       });
   });
+
+  it('should not create todo with invalid body data', (done) => {
+    
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
 });
+
+// describe('GET /todos', () => {
+//   it('should get the todo data', (done) => {
+
+//     request(app)
+//       .get('/todos')
+//       .expect(200)
+//       .expect(())
+
+//   });
+// })
